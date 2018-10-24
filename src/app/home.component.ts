2d6ad99b3d491204638f2961cf1bf250baac50e0
wangping <10196470@zte.intra>
2018-10-24 09:19:33 +0800

    添加轮播图

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit} from '@angular/core';
+declare var Swiper:any;
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['home.component.less']
 })
+
 export class HomeComponent implements OnInit {
   	constructor() { 
     }
   	ngOnInit() {
+      var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
   	}
 }
